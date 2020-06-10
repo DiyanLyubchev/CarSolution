eb6d9a6d393f8bad4c72a84cccd6aae3932cfe9f
Diyan Lyubchev <diyan.lyubchev@gmail.com>
2020-06-10 11:53:51 +0300

    Added kafka adaptor

---
+++ CarMsSolution/KafkaManagerService/Adaptor/KafkaAdaptor.cs
@@ -0,0 +1,66 @@
+﻿using Domain.Model;
+using KafkaManagerService.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+
+namespace KafkaManagerService.Adaptor
+{
+    public class KafkaAdaptor
+    {
+        public string GetQueryProtocol()
+        {
+            string message;
+            KafkaProtocolModel protocol = new KafkaProtocolModel();
+            protocol.Head.Action = "QUERY";
+            protocol.Head.Version = "1";
+
+            message = JsonSerializer.Serialize(protocol);
+
+            return message;
+        }
+
+        internal string GetAddNewProtocol(CarViewModel car)
+        {
+            string message;
+            KafkaProtocolModel protocol = new KafkaProtocolModel();
+            protocol.Head.Action = "ADD_CAR";
+            protocol.Head.Version = "1";
+
+            protocol.Data.Add(new KpCarModel
+            {
+                CarBrand = car.CarBrand,
+                CarModel = car.CarModel,
+                EngineType = car.EngineType
+            });
+
+            message = JsonSerializer.Serialize(protocol);
+
+            return message;
+        }
+
+        public List<CarViewModel> GetCars(string msg)
+        {
+            KafkaProtocolModel kafkaProtocol = this.GetProtocol(msg);
+
+            return Mapp(kafkaProtocol.Data);
+        }
+
+        private List<CarViewModel> Mapp(List<KpCarModel> list)
+        {
+            return list.Select(car => new CarViewModel 
+            {
+                 CarBrand = car.CarBrand,
+                  CarModel =car.CarModel,
+                  EngineType = car.EngineType
+            }).ToList();
+        }
+
+        private KafkaProtocolModel GetProtocol(string msg)
+        {
+            return JsonSerializer.Deserialize<KafkaProtocolModel>(msg);
+        }
+    }
+}
