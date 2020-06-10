0d49457bf9a18646134e355ed3443b5a77adeeb1
Diyan Lyubchev <diyan.lyubchev@gmail.com>
2020-06-10 11:56:29 +0300

    Added kafka adaptor

--- CarMsSolution/KafkaManagerService/Services/KafkaDieselServise.cs
+++ CarMsSolution/KafkaManagerService/Services/KafkaDieselServise.cs
@@ -1,11 +1,14 @@
-﻿using KafkaService;
+﻿using Domain.Contracts;
+using Domain.Model;
+using KafkaService;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace KafkaManagerService.Services
 {
-    public class KafkaDieselServise
+    public class KafkaDieselServise : ICarDieselService
     {
         private KafkaConsumerMenager consumerMenager;
         private KafkaProducerService KafkaProducerService;
@@ -13,6 +16,21 @@ namespace KafkaManagerService.Services
         {
             this.consumerMenager = consumerMenager;
             KafkaProducerService = kafkaProducerService;
+        }
+
+        public Task<int> AddNewCar(CarViewModel carView)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<CarViewModel>> GetDieselCarAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task RemoveDieselCarAsync(int carId)
+        {
+            throw new NotImplementedException();
         }
     }
 }
